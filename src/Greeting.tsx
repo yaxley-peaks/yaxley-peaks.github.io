a5ec9614303fa9c10ba8b13b35095b1f6b389559
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-07-27 20:54:55 +0530

    (* ^ ω ^)

--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -3,20 +3,24 @@ import "./Greeting.css"
 import { Props } from "./PropInterfaces"
 import data from "./data.json"
 
+let msg = data.greeting["1"];
 export default function Greeting(props: Props) {
+    const [count] = props.count!;
     if (props.isMobile) {
         return (
             <div>
                 <h1>Welcome to my page.</h1>
-                <h2 id="mobile">Click Below to Begin!</h2>
+                <h2 id="mobile">{msg}</h2>
                 <h2 id="leave">GO AWAY!</h2>
             </div>
         );
     } else {
+        // @ts-ignore
+        msg = data.greeting[`${count}`] ?? msg;
         return (
             <div>
                 <h1>Welcome to my page.</h1>
-                <h2>Click Below to Begin!</h2>
+                <h2>{msg}</h2>
             </div>
         );
     }
