d6b88c1d6b81f616d71861d4bbd72434368163f3
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-02-02 19:03:17 +0530

    add mathjax support

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const mathjaxPlugin = require("eleventy-plugin-mathjax");
+
 module.exports = function (eleventyConfig) {
     // Return your Object options:
-    eleventyConfig.addPassthroughCopy("css")
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPlugin(mathjaxPlugin);
     return {
         passthroughFileCopy: true,
         dir: {
