18ff5875081b99e6edc98d782eaade39f44d1bd5
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-02-02 18:39:42 +0530

    ok this is working now

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,10 @@ module.exports = function (eleventyConfig) {
     // Return your Object options:
     eleventyConfig.addPassthroughCopy("css")
     return {
-        pathPrefix: "/thing/",
         passthroughFileCopy: true,
         dir: {
             input: "static",
-            output: "public/things"
+            output: "public"
         }
     }
 };
--- build_11ty.sh
+++ build_11ty.sh
@@ -3,4 +3,4 @@
 set -xe
 npx @11ty/eleventy 
 # cp -rf ./static/css/ ./public/things/
-cp -rf ./static/_assets/ ./public/things/
+cp -rf ./static/_assets/ ./public/posts/
