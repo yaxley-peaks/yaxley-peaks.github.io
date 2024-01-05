6c7a6d2a5c4f966d23f4f01dc274efa944bb118a
yaxley-peaks <epiclycoolgaemer@gmail.com>
2024-01-05 20:43:10 +0530

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ import Footer from "./Footer";
 
 export default function App() {
 
+    function isTouchEnabled(): boolean {
+        return (('ontouchstart' in window)
+             || (navigator.maxTouchPoints > 0));
+    }
     let details = navigator.userAgent;
     let regexp = /android|iphone|kindle|ipad/i;
-    let isMobileDevice = regexp.test(details);
+    let isMobileDevice = regexp.test(details) || isTouchEnabled();
+
 
     const count = useState(1);
 
